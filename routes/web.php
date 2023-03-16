1ef557949e49eeee3c45488295652dde39d417c4
Emanuel Aguiar <emanuel_pe@live.com>
2023-03-16 14:08:40 -0300

    add model, controller e migration de products

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('/users', [ UserController::class, "index" ] )->name('users.index');
 Route::get('/users/create', [ UserController::class, "create" ] )->name('users.create');
 Route::post('/users', [ UserController::class, "store" ] )->name('users.store');
 Route::get('/users/{id}/edit', [UserController::class, "edit"] )->name('users.edit');
 Route::put('/users/{id}', [UserController::class, "update"] )->name('users.update');
 Route::delete('/users/{id}', [UserController::class, "destroy"] )->name('users.destroy');
+*/
 
+Route::resource("/users", UserController::class);
+Route::resource("/products", ProductController::class);
